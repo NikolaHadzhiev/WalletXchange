0b1ca3f58b5ed4a3ca87445d359f326007c7a229
NikolaHadzhiev <nikihad2012@abv.bg>
2025-01-22 01:01:21 +0200

    Tried fixing PROD config for hosting server

--- server/server.js
+++ server/server.js
@@ -8,32 +8,36 @@ const ddosProtection = require("./middlewares/ddosRateLimiter");
 const app = express();
 const cookieParser = require('cookie-parser');
 
+// Use Helmet with custom settings
+app.use(
+  helmet({
+    // Content Security Policy (CSP)
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"], // Only allow resources from the same origin
+        scriptSrc: ["'self'"], // Only allow scripts from the same origin
+        objectSrc: ["'none'"], // Prevent all plugins (e.g., Flash, Java applets)
+        connectSrc: ["'self'", process.env.cors_url], // Allow API connections from walletxchange-wallet.vercel.app
+      },
+    },
+
+    // Referrer-Policy Header
+    referrerPolicy: { policy: "no-referrer" }, // No referrer info sent with requests
+
+    // Strict-Transport-Security (HSTS)
+    strictTransportSecurity: {
+      maxAge: 31536000, // 1 year in seconds
+      includeSubDomains: true, // Apply to all subdomains
+    },
+  })
+);
+
 // Allow requests from localhost:3000
 app.use(cors({
   origin: ['http://localhost:3000', process.env.cors_url],
   methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
-}));
-
-// Use Helmet with custom settings
-app.use(helmet({
-  // Content Security Policy (CSP)
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"], // Only allow resources from the same origin (self)
-      scriptSrc: ["'self'"], // Only allow scripts from the same origin (self)
-      objectSrc: ["'none'"], // Prevent all plugins (e.g., Flash, Java applets)
-    },
-  },
-  // Referrer-Policy Header: Controls the amount of information sent with HTTP requests
-  referrerPolicy: { policy: "no-referrer" }, 
-  // "no-referrer" means no referrer information will be sent with requests
-
-  // Strict-Transport-Security (HSTS)
-  strictTransportSecurity: {
-    maxAge: 31536000, // 1 year in seconds (duration to enforce HTTPS)
-    includeSubDomains: true, // Apply this policy to all subdomains
-  },
 }));
 
 app.use(express.json());

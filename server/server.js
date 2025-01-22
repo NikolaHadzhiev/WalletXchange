fef4420d598eb70d85c5070631d40b8c5fe9ccff
NikolaHadzhiev <nikihad2012@abv.bg>
2025-01-22 19:05:47 +0200

    Updated prod environment for hosting with Fly.io

--- server/server.js
+++ server/server.js
@@ -10,35 +10,32 @@ const cookieParser = require('cookie-parser');
 
 // Allow requests from localhost:3000
 app.use(cors({
-  origin: '*',
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
+  origin: process.env.cors_url,
+  ethods: ["GET", "POST", "DELETE", "OPTIONS"],
   credentials: true
 }));
 
 // Use Helmet with custom settings
-// app.use(
-//   helmet({
-//     // Content Security Policy (CSP)
-//     contentSecurityPolicy: {
-//       directives: {
-//         defaultSrc: ["'self'"], // Only allow resources from the same origin
-//         scriptSrc: ["'self'"], // Only allow scripts from the same origin
-//         objectSrc: ["'none'"], // Prevent all plugins (e.g., Flash, Java applets)
-//         connectSrc: ["'self'", "*"], // Allow API connections from walletxchange-wallet.vercel.app
-//       },
-//     },
+app.use(helmet({
+  // Content Security Policy (CSP)
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"], // Only allow resources from the same origin (self)
+      scriptSrc: ["'self'"], // Only allow scripts from the same origin (self)
+      objectSrc: ["'none'"], // Prevent all plugins (e.g., Flash, Java applets)
+    },
+  },
+  // Referrer-Policy Header: Controls the amount of information sent with HTTP requests
+  referrerPolicy: { policy: "no-referrer" }, 
+  // "no-referrer" means no referrer information will be sent with requests
 
-//     // Referrer-Policy Header
-//     referrerPolicy: { policy: "no-referrer" }, // No referrer info sent with requests
+  // Strict-Transport-Security (HSTS)
+  strictTransportSecurity: {
+    maxAge: 31536000, // 1 year in seconds (duration to enforce HTTPS)
+    includeSubDomains: true, // Apply this policy to all subdomains
+  },
+}));
 
-//     // Strict-Transport-Security (HSTS)
-//     strictTransportSecurity: {
-//       maxAge: 31536000, // 1 year in seconds
-//       includeSubDomains: true, // Apply to all subdomains
-//     },
-//   })
-// );
 
 app.use(express.json());
 app.use(cookieParser());

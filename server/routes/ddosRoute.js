c4b2872de727c4f8ac3b9fe950a08644009cee99
NikolaHadzhiev <nikihad2012@abv.bg>
2025-01-23 20:29:36 +0200

    Removed test response

--- server/routes/ddosRoute.js
+++ server/routes/ddosRoute.js
@@ -3,8 +3,6 @@ const LoginAttempt = require("../models/loginAttemptModel"); // Import loginAtte
 
 // Check if the IP is blocked or rate-limited
 router.get("/ddos-check", async (req, res) => {
-  res.json({ message: "CORS is configured!" });
-  
   try {
     // Find the login attempt record for the current IP address
     const loginAttempt = await LoginAttempt.findOne({ identifier: req.ip });
